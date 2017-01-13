57b5e96fe3ad7fedb2dd42cb958bc43efbb580e4
Wolford <lou.wolford.ctr@adlnet.gov>
2017-01-13 09:50:48 -0500

    got all 3 graphs working

--- Tools/StatementGenerator/js/charts.js
+++ Tools/StatementGenerator/js/charts.js
@@ -1,18 +1,17 @@
 var lineDash;
 var barDash;
 var multiBarDash;
-var xData;
-var moduleNames;
 
 function drawLineChart(){
     d3.select('svg').empty();
     var statements = lineDash.data.where(
         'object.id = /(http:\/\/adlnet\.gov\/xapi\/samples\/xapi-jqm\/course\/(01-intro|02-ingredients|03-steps|04-video|05-quiz))$/');
     lineDash.data = statements;
-    var data = formatData(lineDash.data);
-    xData = data[0];
-    moduleNames = data[1];
-    graphLineModules();
+    var datum, moduleNames;
+    var formattedData = formatLineData(lineDash.data);
+    datum = formattedData[0];
+    moduleNames = formattedData[1];
+    graphLineModules(datum, moduleNames);
 }
 
 function drawBarChart(){
@@ -29,10 +28,10 @@ function drawMultiBarChart(){
         'verb.id = /(launched|read)$/ and (' +
         'object.id = /(http:\/\/adlnet\.gov\/xapi\/samples\/xapi-jqm\/course\/(01-intro|02-ingredients|03-steps|04-video|05-quiz)(\/{0,1})(.*))$/)');
     multiBarDash.data = statements;
-    graphMultiBarModules();
+    graphMultiBarModules(formatMultiData(multiBarDash.data));
 }
 
-var graphLineModules = (function() {
+var graphLineModules = (function(datum, moduleNames) {
     var lineChart = nv.models.lineChart()
         .useInteractiveGuideline(true)
         .x(function(d) { return d.x; })
@@ -40,18 +39,18 @@ var graphLineModules = (function() {
         .margin({bottom: 100, right: 50})
     lineChart.xAxis
         .rotateLabels(45)
-        .tickValues(Array.apply(null, {length: xData[0].values.length}).map(Number.call, Number))
+        .tickValues(Array.apply(null, {length: datum[0].values.length}).map(Number.call, Number))
         .tickFormat(function(d){
             return moduleNames[d];
         })
     d3.select('#linechart')
-        .datum(xData)
+        .datum(datum)
         .call(lineChart);
     nv.utils.windowResize(lineChart.update);    
     return lineChart;
 });
 
-function formatData(data) {
+function formatLineData(data) {
     var ret = [{key: "times-launched", values: []}];
     var mod = [];
     var grps = data.orderBy('object.id').groupBy('object.id');
@@ -90,20 +89,85 @@ function graphBarModules() {
     barChart.draw();
 }
 
-function graphMultiBarModules() {
-    var modules = multiBarDash.createMultiBarChart({
-        container: '#multibarchart',
-        groupBy: 'object.id',
-        innerGroupBy: 'verb.id',
-        rangeLabel: 'data.0.object.definition.name.en-US',
-        aggregate:ADL.multiAggregate(ADL.count('verb.id')),
-        customize: function (chart) {
-            chart.xAxis.rotateLabels(45);
-        },
+var graphMultiBarModules = (function(datum) {
+
+    var moduleNames = ['01-intro', '02-ingredients', '03-steps', '04-video', '05-quiz'];
+    var mBarChart = nv.models.multiBarChart()
+        .x(function(d) { return d.x; })
+        .y(function(d) { return d.y; })
+        .margin({bottom: 100, right: 50})
+    mBarChart.xAxis
+        .rotateLabels(45)
+        .tickValues(Array.apply(null, {length: datum[0].values.length}).map(Number.call, Number))
+        .tickFormat(function(d){
+            return moduleNames[d];
+        })
+
+    d3.select('#multibarchart')
+        .datum(datum)
+        .transition()
+        .duration(500)
+        .call(mBarChart);
+    nv.utils.windowResize(mBarChart.update);    
+    return mBarChart;
+});
+
+function formatMultiData(data) {
+    var ret = [{key: "launched", values: []}, {key: "read", values: []}];
+    var grps = data.orderBy('object.id').groupBy('verb.id').groupBy('object.id');
+
+    grps.contents[0].data.map(function(o, i){
+        var idx;
+        switch (o.group){
+            case "http://adlnet.gov/xapi/samples/xapi-jqm/course/01-intro":
+                idx = 0;
+                break;
+            case "http://adlnet.gov/xapi/samples/xapi-jqm/course/02-ingredients":
+                idx = 1;
+                break;
+            case "http://adlnet.gov/xapi/samples/xapi-jqm/course/03-steps":
+                idx = 2;
+                break;
+            case "http://adlnet.gov/xapi/samples/xapi-jqm/course/04-video":
+                idx = 3;
+                break;
+            case "http://adlnet.gov/xapi/samples/xapi-jqm/course/05-quiz":
+                idx = 4;
+                break;
+        }
+        ret[0].values.push({x: idx, y: o.data.length});
     });
-    modules.clear();
-    modules.draw();
-} 
+    
+    var introCount, ingCount, stepCount, vidCount, quizCount;
+    introCount = ingCount = stepCount = vidCount = quizCount = 0;
+    grps.contents[1].data.map(function(o, i){
+        switch (true) {
+            case /(http:\/\/adlnet\.gov\/xapi\/samples\/xapi-jqm\/course\/(01-intro)(\/{0,1})(.*))$/.test(o.group):
+                introCount += o.data.length;
+            break;
+            case /(http:\/\/adlnet\.gov\/xapi\/samples\/xapi-jqm\/course\/(02-ingredients)(\/{0,1})(.*))$/.test(o.group):
+                ingCount += o.data.length; 
+            break;
+            case /(http:\/\/adlnet\.gov\/xapi\/samples\/xapi-jqm\/course\/(03-steps)(\/{0,1})(.*))$/.test(o.group):
+                stepCount += o.data.length;
+            break;
+            case /(http:\/\/adlnet\.gov\/xapi\/samples\/xapi-jqm\/course\/(04-video)(\/{0,1})(.*))$/.test(o.group):
+                vidCount += o.data.length;
+            break;
+            case /(http:\/\/adlnet\.gov\/xapi\/samples\/xapi-jqm\/course\/(05-quiz)(\/{0,1})(.*))$/.test(o.group):
+                quizCount += o.data.length;
+            break;
+        }
+    });
+
+    ret[1].values.push({x: 0, y: introCount});
+    ret[1].values.push({x: 1, y: ingCount});
+    ret[1].values.push({x: 2, y: stepCount});
+    ret[1].values.push({x: 3, y: vidCount});
+    ret[1].values.push({x: 4, y: quizCount});
+
+    return ret;
+}
 
 $(document).ready(function() {
     ADL.launch(function(err, launchdata, xAPIWrapper) {
@@ -153,4 +217,5 @@ $( "#load-graphs" ).click(function() {
             },
             function(){drawMultiBarChart();}
     );
+  
 });
